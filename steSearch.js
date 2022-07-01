ab1684a6ca110111b69fdae5f7f1a7d41c8d7b0a
Andrew Gibb <amg10@hw.ac.uk>
2022-07-01 20:42:28 +0100

    F27IP - Adding hyperlink functionality.

--- steSearch.js
+++ steSearch.js
@@ -1,17 +1,18 @@
+// click searchButton on pressing enter
+let inputBox = document.getElementById("searchWord");
+inputBox.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        document.getElementById("searchButton").click();
+    }
+    });
+
 function steSearch() {
 
+    // clear table each time function is called
     document.getElementById("tablediv").innerHTML = "";
 
     // array of table headers
     var header = ["Word", "Approved", "Part of speech", "Approved meaning/ alternatives", "Approved example", "Not approved example"];
-    
-    // click searchButton on pressing enter
-    let inputBox = document.getElementById("searchWord");
-    inputBox.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-          document.getElementById("searchButton").click();
-        }
-      });
 
     // store input from searchWord in a variable 
     let input = inputBox.value;
@@ -32,7 +33,7 @@ function steSearch() {
         for (j = 0; j < header.length; j++){
             let th = document.createElement("th");
             th.innerHTML = header[j];
-            th.classList.add("tableHead")
+            th.classList.add("tableHead");
             headrow.appendChild(th);
         }
 
@@ -52,23 +53,28 @@ function steSearch() {
                     if(ste[i].word.includes(input)){
 
                         let trow = document.createElement("tr");
-                        trow.classList.add("tableRow");
                         tbody.appendChild(trow);
 
                         var entry = ste[i];
 
-                        let keys = Object.keys(ste[i]);
-
                         for (k in entry) {
                             let td = document.createElement("td");
-                            td.innerHTML = entry[k];
+
+                            // set id to the key (k)
+                            td.setAttribute("id", k);
+
+                            // if the key is "word", create a hyperlink to dictionary.com
+                            if (td.id == "word") {
+                                let a = document.createElement('a');
+                                a.href = "https://www.dictionary.com/browse/" + entry[k];
+                                a.innerText = entry[k];
+                                td.appendChild(a);
+                            } else td.innerHTML = entry[k];
 
                                 // if statement to check if word is approved
                                 if(ste[i].approved){
                                     td.classList.add("wordApproved");
                                 } else td.classList.add("wordBanned");
-
-                            td.classList.add("tableData");
 
                             trow.appendChild(td);
                         }
