c1b66336eea752c85cafc3f72be96dad56c62782
Andrew Gibb <amg10@hw.ac.uk>
2022-07-02 19:10:22 +0100

    F27IR - Various, update JSON.

--- steSearch.js
+++ steSearch.js
@@ -9,53 +9,19 @@ inputBox.addEventListener("keypress", function (event) {
 function steSearch() {
 
     // clear table each time function is called
-    document.getElementById("tablediv").innerHTML = "";
+    document.getElementById("tbody").innerHTML = "";
+
+    // clear not found function
+    document.getElementById("notFound").innerHTML = "";
 
     // store input from searchWord in a variable as lowercase
     let input = inputBox.value.toLowerCase();
 
+    // check for empty input
     if (input != "") {
 
-        // create table and header
-        createTable();
-
         // fetch JSON file and create table body
-        createBody();
-
-        // check if no words have been found
-        notFound(input);
-
-    }
-
-    function createTable() {
-
-        // array of table headers
-        var header = ["Word", "Approved", "Part of speech", "Approved meaning/ alternatives", "Approved example", "Not approved example"];
-
-        let table = document.createElement("table");
-        table.setAttribute("id", "table");
-        document.getElementById("tablediv").appendChild(table);
-
-        // create table header
-        let thead = document.createElement("thead");
-        table.appendChild(thead);
-        let headrow = document.createElement("tr");
-        thead.appendChild(headrow);
-
-        // loop through header array to create table headers
-        for (j = 0; j < header.length; j++) {
-            let th = document.createElement("th");
-            th.innerHTML = header[j];
-            th.classList.add("tableHead");
-            headrow.appendChild(th);
-        }
-
-        // create table body
-        let tbody = document.createElement("tbody");
-        tbody.setAttribute("id", "tbody");
-        table.appendChild(tbody);
-
-        return table;
+        createBody();        
     }
 
     function createBody() {
@@ -68,6 +34,11 @@ function steSearch() {
                 // search for matching word and create result table row
                 for (i = 0; i < ste.length; i++) {
                     if (ste[i].word.includes(input)) {
+
+                        // // create table body
+                        // let tbody = document.createElement("tbody");
+                        // tbody.setAttribute("id", "tbody");
+                        // table.appendChild(tbody);
 
                         let trow = document.createElement("tr");
                         document.getElementById("tbody").appendChild(trow);
@@ -98,24 +69,14 @@ function steSearch() {
 
                             trow.appendChild(td);
                         }
-
-
                     }
+                }
+
+                // check if no words have been found and display message. If no words haave been found, there will be no elements with the "word" class
+                if (document.getElementsByClassName("word").length == 0){
+                    let capsword = String(input).charAt(0).toUpperCase() + String(input).slice(1);
+                    document.getElementById("notFound").innerHTML = capsword + " is not found in the terminology database. Please enter another word.";
                 }
             });
     }
-
-    function notFound(input) {
-        let wordFound = document.getElementsByClassName('word').length > 0;
-        if (wordFound){
-            document.getElementById("notFound").innerHTML = wordFound;
-            // caps(input) + " is not found in the terminology database. Please enter another word.";
-        }
-
-    }
-
-    function caps(input){
-        return input.charAt[0].toUpperCase() + input.slice[1];
-    }
-
 }
